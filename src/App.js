92c673d5dc8ded32f080421194dbd09290403c0c
Claudio Matos <claudiomatos@gmail.com>
2024-03-23 11:57:03 -0300

    Pokemon Card Concluído usando API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React, {useEffect, useState} from 'react';
+import './style.css';
 
 function App() {
+  const [pokemon, setPokemon] = useState([]);
+  const [input, setInput] = useState('');
+
+function loadAPI(pokeName){
+  let url = `https://pokeapi.co/api/v2/pokemon/${pokeName}`;
+  fetch(url)
+  .then(response => response.json())
+  .then(res => {
+    console.log(res)
+    setPokemon(res)
+  })
+  .catch(err => console.log(err));
+}
+
+  useEffect(() => {
+
+  }, []);
+
+    function handleConsulta(e) {
+        e.preventDefault();
+          loadAPI(input);
+          setInput('');
+        }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className = 'container'>
+      <header>
+        <strong>Pokemon API</strong>
       </header>
+
+      <form onSubmit={handleConsulta}>
+        <input className='caixa' placeholder="Informe o nome do pokemon: " value={input} onChange={(e) => setInput(e.target.value)} />
+        <br />
+        <button type="submit"> Consultar </button>
+      </form>
+
+      <div className='card'>
+        <img src={pokemon.sprites?.front_default} alt={pokemon.name} />
+        <div>Name: {pokemon.name} </div>
+        <div>Nº: {pokemon.id} </div>
+        <div>Peso: {pokemon.weight / 10}kg</div>
+        <div>Altura: {pokemon.height / 10}m</div>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default App
